10274b6dded43c226740b3cc485652c3f5ecbdee
Julian Dano <julian.dano2016@gmail.com>
2018-06-14 22:12:33 -0400

    formatted footer section / added smooth scroll / minor css updates

--- Assets/js/custom.js
+++ Assets/js/custom.js
@@ -34,5 +34,29 @@ $(document).ready(function() {
 		
 	});
 	
+	  // Add smooth scrolling to all links
+	  $(".smoothScroll").on('click', function(event) {
+
+	    // Make sure this.hash has a value before overriding default behavior
+	    if (this.hash !== "") {
+	      // Prevent default anchor click behavior
+	      event.preventDefault();
+
+	      // Store hash
+	      var hash = this.hash;
+
+	      // Using jQuery's animate() method to add smooth page scroll
+	      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+	      var anchor = $(hash).offset().top - 60;
+	      $('html, body').animate({
+	        scrollTop: anchor
+	      }, 800, function(){
+
+	        // Add hash (#) to URL when done scrolling (default click behavior)
+	        // window.location.hash = hash;
+	      });
+	    } // End if
+	  })
+	
 	
 });
