996a66242c801927b483a1f1952e75f96f7e7876
Julian Dano <julian.dano2016@gmail.com>
2018-06-18 21:38:45 -0400

    services tabs fade and slide / stacked work-with-us elements / fixed footer hours

--- Assets/js/custom.js
+++ Assets/js/custom.js
@@ -5,30 +5,36 @@ $(document).ready(function() {
 		
 		var firstTab = $('#services-section .nav-tabs')[0].firstElementChild;
 		var lastTab = $('#services-section .nav-tabs')[0].lastElementChild;
-
+		var activeTabHash = $('#services-section .nav-tabs li.active a')[0].hash;
+		console.log(activeTabHash);
 		
 		if ( e.currentTarget.classList.contains('serv-navTab-left') ) {
 			
 			var previousSibling = $('#services-section .nav-tabs li.active')[0].previousElementSibling;
-			var lastSibling = $('#services-section .nav-tabs li.active')[0].previousElementSibling;
-
+//			$(activeTabHash).css("opacity", "0");
+			
 			if ( $(previousSibling).length === 1 ) {
-				$(previousSibling).find('a').trigger('click');
+//				$(previousSibling).find('a').trigger('click');
+				$(previousSibling).find('a').tab('show');
 			}
 			else {
-				$(lastTab).find('a').trigger('click');
+//				$(lastTab).find('a').trigger('click');
+				$(lastTab).find('a').tab('show');
 			}
 			
 		}
 		else if ( e.currentTarget.classList.contains('serv-navTab-right') ) {
 			
 			var nextSibling = $('#services-section .nav-tabs li.active')[0].nextElementSibling;
-
+//			$(activeTabHash).css("opacity", "0");
+			
 			if ( $(nextSibling).length === 1 ) {
-				$(nextSibling).find('a').trigger('click');
+//				$(nextSibling).find('a').trigger('click');
+				$(nextSibling).find('a').tab('show');
 			}
 			else {
-				$(firstTab).find('a').trigger('click');
+//				$(firstTab).find('a').trigger('click');
+				$(firstTab).find('a').tab('show');
 			}
 		}
 		
