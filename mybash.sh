e8b7807db9ac894c940d4c0918decaf9c4ce5ca3
Julian Dano <julian.dano2016@gmail.com>
2018-07-27 21:02:37 -0400

    updated bash file

--- mybash.sh
+++ mybash.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-php index.php > index.html
+DEST="./public_html"
+
+# Run PHP on gig folders and output HTML ready for static hosting
+find . -maxdepth 1 -type d \( -name '*-????' -o -name sample \) | sed 's/.\///' | \
+while read x; do \
+    mkdir -p "$DEST/$x/" && cd "$x" && php index.php > "../$DEST/$x/index.html" && cd ../ && \
+    echo "Processed $x" ; \
+done
